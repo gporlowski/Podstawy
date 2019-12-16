4399baec005131d007888de3e02418a01a76d05b
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 20:49:54 +0100

    Podstawy 3

---
+++ Podstawy3.java
@@ -0,0 +1,12 @@
+public class Podstawy3 {
+    public static void main(String[] args) {
+        int number = -21;
+        if (number > 0) {
+            System.out.println("Liczba (" + number + ") jest większa od 0.");
+        } else if (number < 0){
+            System.out.println("Liczba (" + number + ") jest mniejsza od 0.");
+        } else {
+            System.out.println("Liczba jest równa 0.");
+        }
+    }
+}
