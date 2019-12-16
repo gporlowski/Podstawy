3f1b2f8f1e58e6c8cb7b1bd3bfc815d9a8adb884
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 20:49:21 +0100

    Podstawy 2

---
+++ Podstawy2.java
@@ -0,0 +1,10 @@
+public class Podstawy2 {
+    public static void main(String[] args) {
+        int number = 73;
+        if (number % 2 == 0) {
+            System.out.println("Liczba (" + number + ") jest parzysta.");
+        } else {
+            System.out.println("Liczba (" + number + ") nie jest parzysta.");
+        }
+    }
+}
